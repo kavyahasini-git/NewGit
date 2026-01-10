b27704a036f73d2976d5205698ec31866198aea1
kavyahasini-git <kavya.chandu89@gmail.com>
2026-01-10 19:27:03 +0530

    Committing my Changes

---
+++ Numbercount.java
@@ -0,0 +1,18 @@
+package interview;
+
+public class Numbercount {
+
+	public static void main(String[] args) {
+		int num = 12345;
+		
+		int count=0;
+		
+		while(num!=0) {
+			num=num/10;
+			count++;
+			
+		}System.out.println(count);
+
+	}
+
+}
